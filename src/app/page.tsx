bb26456f128060dd5975b753da2967269b75d574
JAYBORICHA <jayboricha707@gmail.com>
2023-12-31 12:00:15 +0530

    Started Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,84 @@
-import Image from 'next/image'
+/* eslint-disable @next/next/no-img-element */
+"use client"
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+
+  const router = useRouter();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-screen w-full">
+      <section className='flex flex-col h-full w-full justify-center items-center p-4'>
+        <div className='rounded-full bg-orange-300 w-fit p-1 m-2 px-5 mb-6'>
+          <p className='font-bold'>Ultimate accountability platform</p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <p className='text-6xl font-bold max-w-5xl'>AccountaMate - Achieve Your</p>
+        <p className='text-6xl font-bold max-w-3xl mb-7'>Goals with Accountability</p>
+        <p className='font-bold max-w-6xl'>Welcome to AccountaMate, the ultimate accountability platform that turns your goals into achievements! Discover how our unique system</p>
+        <p className='font-semibold max-w-7xl mb-7'>you reach your targets with the power of accountability can keep you on track and help</p>
+        <div >
+          <Button size={'lg'} onClick={() => (router.push('#activities'))}>Set your first goal now</Button>
+        </div>
+      </section>
+      <section id='activities' className='flex justify-center  w-full gap-3'>
+        <div className='grid grid-cols-3 grid-flow-row'>
+          <figure className="max-w-sm flex justify-center items-center transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+              <img className="rounded-lg hover:opacity-80" src='/images/travelv2.jpeg' alt="image description" />
+              <figcaption className="absolute px-4 text-4xl font-extrabold text-white">
+              <p>Travell world</p>
+            </figcaption>
+          </figure>
+          <figure className="max-w-sm flex justify-center items-center transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+              <img className="rounded-lg hover:opacity-80" src='/images/codingv1.jpeg' alt="image description" />
+              <figcaption className="absolute px-4 text-4xl font-extrabold text-white">
+              <p>Code Everyday</p>
+            </figcaption>
+          </figure>
+          <figure className="max-w-sm flex justify-center items-center transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+              <img className="rounded-lg hover:opacity-80" src='/images/coffeev0.jpeg' alt="image description" />
+              <figcaption className="absolute px-4 text-4xl font-extrabold text-white">
+              <p>Quit Coffee</p>
+            </figcaption>
+          </figure>
+          <figure className="max-w-sm flex justify-center items-center transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+              <img className="rounded-lg hover:opacity-80" src='/images/cookingv0.jpeg' alt="image description" />
+              <figcaption className="absolute px-4 text-4xl font-extrabold text-white">
+              <p>Cook Everyday</p>
+            </figcaption>
+          </figure>
+          <figure className="max-w-sm flex justify-center items-center transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+              <img className="rounded-lg hover:opacity-80" src='/images/weightlossv1.jpeg' alt="image description" />
+              <figcaption className="absolute px-4 text-4xl font-extrabold text-white">
+              <p>Loss 10kg</p>
+            </figcaption>
+          </figure>
+          <figure className="max-w-sm flex justify-center items-center transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+              <img className="rounded-lg hover:opacity-80" src='/images/gymv0.jpeg' alt="image description" />
+              <figcaption className="absolute px-4 text-4xl font-extrabold text-white">
+              <p>Hit Gym</p>
+            </figcaption>
+          </figure>
+          <figure className="max-w-sm flex justify-center items-center transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+              <img className="rounded-lg hover:opacity-80" src='/images/journelv0.jpeg' alt="image description" />
+              <figcaption className="absolute px-4 text-4xl font-extrabold text-white">
+              <p>Write Journal</p>
+            </figcaption>
+          </figure>
+          <figure className="max-w-sm flex justify-center items-center transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+              <img className="rounded-lg hover:opacity-80" src='/images/running.jpeg' alt="image description" />
+              <figcaption className="absolute px-4 text-4xl font-extrabold text-white">
+              <p>Run 5km</p>
+            </figcaption>
+          </figure>
+          <figure className="max-w-sm flex justify-center items-center transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+              <img className="rounded-lg hover:opacity-80" src='/images/startupv0.jpeg' alt="image description" />
+              <figcaption className="absolute px-4 text-4xl font-extrabold text-white">
+              <p>Build Startup</p>
+            </figcaption>
+          </figure>
+        </div>
+      </section>
+    </div>
   )
 }
