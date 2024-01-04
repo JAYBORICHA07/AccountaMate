060120c2a8f82ded5817608ce6c3c2eba6157337
JAYBORICHA <jayboricha707@gmail.com>
2024-01-05 00:31:22 +0530

    Compeleted mogration from mongoDB to supabase backend entrie are being done in the database sucessfully

--- src/app/api/users/challenge/route.ts
+++ src/app/api/users/challenge/route.ts
@@ -5,9 +5,27 @@ import supabase from "../../../../../dbConfig/dbConfig";
 
 export async function POST(request : NextRequest){
     try {
-            const { data , error} = await supabase.from("accountmate").select()
-            console.log("data")
-            console.log({data})
+            const reqBody = await request.json();
+            const {userName, userEmail, friendsEmail, friendsName, amount, goal} = reqBody
+
+
+
+
+
+            const { data , error} = await supabase
+                                            .from("accountmate")
+                                            .insert([{
+                                                user_name : userName,
+                                                user_email : userEmail,
+                                                friend_name : friendsName,
+                                                friend_email : friendsEmail,
+                                                goal : goal,
+                                                amount : amount
+                                            }])
+                                            .select()
+            if(error) console.log(error);
+            console.log(data);
+
 
         return NextResponse.json({
             sucess : true,
