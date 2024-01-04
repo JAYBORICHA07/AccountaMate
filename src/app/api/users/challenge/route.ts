b4526d5ffa7e1d74f9e7ef1348076ca081e5673c
JAYBORICHA <jayboricha707@gmail.com>
2024-01-05 00:32:31 +0530

    code formating

--- src/app/api/users/challenge/route.ts
+++ src/app/api/users/challenge/route.ts
@@ -3,36 +3,29 @@ import supabase from "../../../../../dbConfig/dbConfig";
 
 
 
-export async function POST(request : NextRequest){
+export async function POST(request: NextRequest) {
     try {
-            const reqBody = await request.json();
-            const {userName, userEmail, friendsEmail, friendsName, amount, goal} = reqBody
-
-
-
-
-
-            const { data , error} = await supabase
-                                            .from("accountmate")
-                                            .insert([{
-                                                user_name : userName,
-                                                user_email : userEmail,
-                                                friend_name : friendsName,
-                                                friend_email : friendsEmail,
-                                                goal : goal,
-                                                amount : amount
-                                            }])
-                                            .select()
-            if(error) console.log(error);
-            console.log(data);
-
-
+        const reqBody = await request.json();
+        const { userName, userEmail, friendsEmail, friendsName, amount, goal } = reqBody
+        const { data, error } = await supabase
+            .from("accountmate")
+            .insert([{
+                user_name: userName,
+                user_email: userEmail,
+                friend_name: friendsName,
+                friend_email: friendsEmail,
+                goal: goal,
+                amount: amount
+            }])
+            .select()
+        if (error) console.log(error);
+        console.log(data);
         return NextResponse.json({
-            sucess : true,
-            msg : 'Challenge created successfully'
+            sucess: true,
+            msg: 'Challenge created successfully'
         })
 
-    } catch (error : any) {
+    } catch (error: any) {
         console.log("Error handeling request" + error.message)
 
     }
